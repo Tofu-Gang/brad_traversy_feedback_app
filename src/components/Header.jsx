f7e5b0acb53dc64d27aef472af7ac6c8784bab79
Jakub Franěk <tofugangsw@gmail.com>
2024-10-12 20:29:27 +0200

    13. Adding Styles To A Component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,18 @@
 import PropTypes from "prop-types";
 
-function Header({ text = "Feedback UI" }) {
+function Header(
+  {
+    text = "Feedback UI",
+    bgColor = "rgba(0, 0, 0, 0.4)",
+    textColor = "#ff6a95"
+  }) {
+  const headerStyles = {
+    backgroundColor: bgColor,
+    color: textColor
+  };
+
   return (
-    <header>
+    <header style={headerStyles}>
       <div className="container">
         <h2>{text}</h2>
       </div>
@@ -11,7 +21,9 @@ function Header({ text = "Feedback UI" }) {
 }
 
 Header.propTypes = {
-  text: PropTypes.string
+  text: PropTypes.string,
+  bgColor: PropTypes.string,
+  textColor: PropTypes.string
 };
 
 export default Header;
