44afab6620fe91c45ae30a5e9a421dd290008641
Jakub Franěk <tofugangsw@gmail.com>
2024-10-12 20:44:26 +0200

    Clean the button that was connected to rating useState callback.

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -3,18 +3,11 @@ import { useState } from "react";
 function FeedbackItem() {
   const [rating, setRating] = useState(7);
   const [text, setText] = useState("This is an example of a feedback item.");
-  const handleClick = () => {
-    setRating((prev) => {
-      return prev + 1;
-    });
-    setText("The text was updated.");
-  }
 
   return (
     <div className="card">
       <div className="num-display">{rating}</div>
       <div className="text-display">{text}</div>
-      <button onClick={handleClick}>Click</button>
     </div>
   );
 }
