8a843c7f6db57f87c405c20875c1c7e542b6b55d
Jakub Franěk <tofugangsw@gmail.com>
2024-10-13 16:22:27 +0200

    16. Card Component & Conditional Styles

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,5 @@
 import FeedbackItem from "./FeedbackItem";
+import PropTypes from "prop-types";
 
 function FeedbackList({feedback}) {
   if(!feedback || feedback.length === 0) {
@@ -13,5 +14,13 @@ function FeedbackList({feedback}) {
     );
   }
 }
+
+FeedbackList.propTypes = {
+  feedback: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    text: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired
+  }))
+};
 
 export default FeedbackList;
