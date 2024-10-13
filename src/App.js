37149f4dc5d134f9319899d76207ba87507e06e4
Jakub Franěk <tofugangsw@gmail.com>
2024-10-13 17:05:12 +0200

    Section 3: Components, Props & State

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackStats from "./components/FeedbackStats";
+import { useState } from "react";
+import FeedbackData from "./data/FeedbackData";
+
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
+  const deleteFeedback = (itemId) => {
+    if(window.confirm("Are you sure you want to delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== itemId));
+    }
+  }
+
   return (
-    <div className="container">
-      <h1>My App</h1>
-    </div>
+    <>
+      <Header />
+      <div className="container">
+        <FeedbackStats feedback={ feedback } />
+        <FeedbackList feedback={ feedback } handleDelete={ deleteFeedback }/>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/FeedbackStats.jsx
@@ -0,0 +1,24 @@
+import PropTypes from "prop-types";
+
+function FeedbackStats({ feedback }) {
+  const average = Number((feedback.reduce(
+    (ratingsSum, item) => ratingsSum + item.rating, 0) / feedback.length)
+    .toFixed(1).replace(/[.,]0$/, ""));
+
+  return (
+    <div className="feedback-stats">
+      <h4>{feedback.length} Reviews</h4>
+      <h4>Average Rating: { isNaN(average) ? 0 : average }</h4>
+    </div>
+  );
+}
+
+FeedbackStats.propTypes = {
+  feedback: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    text: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired
+  })).isRequired
+};
+
+export default FeedbackStats;
