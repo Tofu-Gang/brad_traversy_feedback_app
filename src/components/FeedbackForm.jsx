10d583329ab0ee5e58755c1d78b0aaebaf87acac
Jakub Franěk <tofugangsw@gmail.com>
2024-10-14 23:23:13 +0200

    22. Rating Select Component

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import RatingSelect from "./RatingSelect";
 import Card from "./shared/Card";
 import Button from "./shared/Button";
 
@@ -6,6 +7,7 @@ function FeedbackForm() {
   const [text, setText] = useState("");
   const [btnDisabled, setBtnDisabled] = useState(true);
   const [message, setMessage] = useState("");
+  const [rating, setRating] = useState(10);
 
   const handleTextChange = (event) => {
     setText(event.target.value);
@@ -26,7 +28,7 @@ function FeedbackForm() {
     <Card>
       <form>
         <h2>How would you rate your service with us?</h2>
-        {/* @todo - rating select component */}
+        <RatingSelect select={(rating) => setRating(rating)}/>
         <div className="input-group">
           <input
             onChange={handleTextChange}
