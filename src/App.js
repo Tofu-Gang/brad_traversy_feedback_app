b9a1b24a3fec62214dbedd70b9bc8b5dfe59bb63
Jakub Franěk <tofugangsw@gmail.com>
2024-10-14 22:13:02 +0200

    19. Form Input & State

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
 import { useState } from "react";
 import FeedbackData from "./data/FeedbackData";
 
@@ -16,6 +17,7 @@ function App() {
     <>
       <Header />
       <div className="container">
+        <FeedbackForm />
         <FeedbackStats feedback={ feedback } />
         <FeedbackList feedback={ feedback } handleDelete={ deleteFeedback }/>
       </div>
